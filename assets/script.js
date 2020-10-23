ace5510a2a198b01a00fafe45034b36436df43df
Santiago Hincapie <santiagohr81@hotmail.com>
2020-10-22 22:01:28 -0400

    the logic for the weather cards updated

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     var city = $("#cityInput").val().trim();
     console.log(city);
     weatherGet(city);
+
+    $("#cityInput").val("");
   });
   var APIKey = "485a30f7b8cebea0111006986b00bf18";
 
@@ -19,9 +21,33 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       console.log(response);
+
+      var currentDay = response.dt;
+
+      var card = $("<div>").addClass("card col-4");
+      var cardBody = $("<div>").addClass("card-body currentWeather");
+      var cardTitle = $("<div>")
+        .addClass("card-title")
+        .text(moment.unix(currentDay).format("dddd"));
+
+      var cityDiv = $("<div>").addClass("card-text").text(city);
+      var tempDiv = $("<div>")
+        .addClass("card-text")
+        .text("Temperature: " + Math.floor(response.main.temp) + "°F");
+      var humDiv = $("<div>")
+        .addClass("card-text")
+        .text("Humidity " + Math.floor(response.main.humidity));
+
+      $("#currentWeather")
+        .empty()
+        .append(
+          card.append(
+            cardBody.append(cardTitle).append(cityDiv, tempDiv, humDiv)
+          )
+        );
+
       forcastGet(response.coord.lat, response.coord.lon);
       uvGet(response.coord.lat, response.coord.lon);
-
     });
   }
   function forcastGet(lat, lon) {
@@ -33,6 +59,8 @@ $(document).ready(function () {
     }).then(function (responseF) {
       console.log(responseF);
 
+      $("#forecast").empty();
+
       for (var i = 1; i < 6; i++) {
         console.log(responseF.daily[i]);
         var currentDay = responseF.daily[i];
@@ -43,7 +71,17 @@ $(document).ready(function () {
           .addClass("card-title")
           .text(moment.unix(currentDay.dt).format("dddd"));
 
-        $("#forecast").append(card.append(cardBody.append(cardTitle)));
+        var tempDiv = $("<div>")
+          .addClass("card-text")
+          .text("Temperature: " + Math.floor(currentDay.temp.day) + "°F");
+        var humDiv = $("<div>")
+          .addClass("card-text")
+          .text("Humidity " + Math.floor(currentDay.humidity));
+        var uviC = $("<div>").text("UV Index " + currentDay.uvi);
+
+        $("#forecast").append(
+          card.append(cardBody.append(cardTitle).append(tempDiv, humDiv, uviC))
+        );
       }
     });
   }
@@ -56,6 +94,8 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (responseU) {
       console.log(responseU);
+      var uviC = $("<div>").text("UV Index " + responseU.value);
+      $(".currentWeather").append(uviC);
     });
   }
 });
