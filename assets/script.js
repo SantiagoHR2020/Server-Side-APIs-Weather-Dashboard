638fc7fb7fd00421713dc88837926738bc41a2cb
Santiago Hincapie <santiagohr81@hotmail.com>
2020-10-24 09:10:14 -0400

    style update

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+
+  var cityHistoryArray = [];
+  cityHistoryArray = JSON.parse(localStorage.getItem("cities"));
+  console.log(cityHistoryArray)
+
   $("#searchBtn").on("click", function (event) {
     event.preventDefault();
-    var city = $("#cityInput").val().trim();
-    console.log(city);
-    weatherGet(city);
+    var city = $("#cityInput").val().toLowerCase().trim();
 
-    $("#cityInput").val("");
+    weatherGet(city);
+    cityHistory(city);
   });
   var APIKey = "485a30f7b8cebea0111006986b00bf18";
 
@@ -20,29 +24,30 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
+      
 
       var currentDay = response.dt;
 
-      var card = $("<div>").addClass("card col-4");
+      var card = $("<div>").addClass("card col-sm-12 col-md-5");
       var cardBody = $("<div>").addClass("card-body currentWeather");
       var cardTitle = $("<div>")
         .addClass("card-title")
-        .text(moment.unix(currentDay).format("dddd"));
+        .text(moment.unix(currentDay).format("MMM Do"));
 
-      var cityDiv = $("<div>").addClass("card-text").text(city);
+      var cityDiv = $("<div>").addClass("card-text").text("City: "+city);
       var tempDiv = $("<div>")
         .addClass("card-text")
         .text("Temperature: " + Math.floor(response.main.temp) + "°F");
       var humDiv = $("<div>")
         .addClass("card-text")
-        .text("Humidity " + Math.floor(response.main.humidity));
+        .text("Humidity: " + Math.floor(response.main.humidity)+"%");
+      var windSpeed = $("<div>").text("Wind speed: "+ response.wind.speed +" MPH")
 
       $("#currentWeather")
         .empty()
         .append(
           card.append(
-            cardBody.append(cardTitle).append(cityDiv, tempDiv, humDiv)
+            cardBody.append(cardTitle).append(cityDiv, tempDiv, humDiv, windSpeed)
           )
         );
 
@@ -57,33 +62,44 @@ $(document).ready(function () {
       url: queryURLF,
       method: "GET",
     }).then(function (responseF) {
-      console.log(responseF);
+      
 
       $("#forecast").empty();
 
       for (var i = 1; i < 6; i++) {
-        console.log(responseF.daily[i]);
+        
         var currentDay = responseF.daily[i];
 
-        var card = $("<div>").addClass("card col-2");
+        var card = $("<div>").addClass("card col-sm-8 col-md-6 col-lg-2");
         var cardBody = $("<div>").addClass("card-body");
         var cardTitle = $("<div>")
           .addClass("card-title")
-          .text(moment.unix(currentDay.dt).format("dddd"));
+          .text(moment.unix(currentDay.dt).format("MMM Do"));
 
         var tempDiv = $("<div>")
           .addClass("card-text")
-          .text("Temperature: " + Math.floor(currentDay.temp.day) + "°F");
+          .text("Temp: " + Math.floor(currentDay.temp.day) + "°F");
         var humDiv = $("<div>")
           .addClass("card-text")
-          .text("Humidity " + Math.floor(currentDay.humidity));
-        var uviC = $("<div>").text("UV Index " + currentDay.uvi);
-
+          .text("Humidity: " + Math.floor(currentDay.humidity)+"%");
+        var uviC = $("<div>").text("UVI: ");
+        var newSpan = $("<span>").addClass("dangerr").text(currentDay.uvi)
         $("#forecast").append(
-          card.append(cardBody.append(cardTitle).append(tempDiv, humDiv, uviC))
+          card.append(cardBody.append(cardTitle).append(tempDiv, humDiv, uviC.append(newSpan)))
         );
+
+        if (parseInt(currentDay.uvi) > 5) {
+          $(".dangerr").attr('style', "background-color : red")
+      } else {
+          $(".dangerr").attr('style', "background-color : green")
+      }
+
       }
     });
+
+
+
+    
   }
 
   function uvGet(lat, lon) {
@@ -93,9 +109,36 @@ $(document).ready(function () {
       url: queryURLU,
       method: "GET",
     }).then(function (responseU) {
-      console.log(responseU);
-      var uviC = $("<div>").text("UV Index " + responseU.value);
-      $(".currentWeather").append(uviC);
+      
+      
+      var uviC = $("<div>").text("UVI ");
+      var newSpan = $("<span>").addClass("dangerr").text(responseU.value)
+
+      $(".currentWeather").append(uviC.append(newSpan));
+
+      if (parseInt(responseU.value) > 5) {
+        newSpan.attr('style', "background-color : red")
+    } else {
+        newSpan.attr('style', "background-color : green")
+    }
     });
   }
+
+  function cityHistory(city) {
+    $("#cityInput").val("")
+    cityHistoryArray.push(city);
+
+    localStorage.setItem("cities", JSON.stringify(cityHistoryArray));
+    
+    $(".input-group").empty();
+    cityHistoryArray.forEach(function(city){
+      var newDiv = $("<button>").addClass("btmCity").attr("id", city).text(city)
+      $(".input-group").append(newDiv)
+    })
+    
+
+  }
 });
+
+
+
